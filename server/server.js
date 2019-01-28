de5b2d4161ac43ecd62b78018961360457722735
Chris Eddy <ceddy.zen@gmail.com>
2019-01-27 22:46:43 -0800

    test react button for DB fetch

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const DB_PASS = process.env.DB_PASS;
 const sequelize = new Sequelize('postgres://master:'+DB_PASS+'@mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com:5432/postgres');
 
 app.get('/', (req, res) => res.send('Welcome to My Living City!'))
+
 app.get('/dbtest', (req, res) => {
   sequelize.query("SELECT message FROM test", { type: sequelize.QueryTypes.SELECT})
   .then(message => {
