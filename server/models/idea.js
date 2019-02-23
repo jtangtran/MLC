369d6ccd38a9bd542492c9a8817f44cc4c771828
Paul Sajna <sajattack@gmail.com>
2019-02-23 15:37:29 -0800

    create an Idea model

---
+++ server/models/idea.js
@@ -0,0 +1,30 @@
+const DB_PASS = process.env.DB_PASS;
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize("mylivingcity", "master", DB_PASS, {
+  dialect: "postgres",
+  host: "mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com",
+  query: { searchPath: "prod", supportsSearchPath: true }
+});
+
+const Idea = sequelize.define('idea', 
+  {
+    id: { 
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: Sequelize.STRING,
+    place_petal: Sequelize.STRING,
+    water_petal: Sequelize.STRING,
+    energy_petal: Sequelize.STRING,
+    health_petal: Sequelize.STRING,
+    materials_petal: Sequelize.STRING,
+    equity_petal: Sequelize.STRING,
+    beauty_petal: Sequelize.STRING
+  },
+  {
+    schema: "prod"
+  }
+);
+
+module.exports = Idea;
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ app.use(cors())
 const port = 3000
 const DB_PASS = process.env.DB_PASS;
 
+const Idea = require('./models/idea')
+
 // DEV DB CONNECTION
 // const sequelize = new Sequelize('postgres', 'postgres', '#HelloThere69', {
 //   host: 'localhost',
@@ -15,15 +17,27 @@ const DB_PASS = process.env.DB_PASS;
 // });
 
 // PRODUCTION DB CONNECTION
-const sequelize = new Sequelize('postgres://master:'+DB_PASS+'@mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com:5432/postgres');
+const sequelize = new Sequelize("mylivingcity", "master", DB_PASS, {
+  dialect: "postgres",
+  host: "mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com",
+  query: { searchPath: "prod", supportsSearchPath: true }
+});
 
 app.get('/', (req, res) => res.send('Welcome to My Living City!'))
 
+app.get('/create_tables', (req, res) => {
+  try {
+    Idea.sync()
+    res.status(200).end();
+  } catch(e){
+    console.log(e.stack);
+    res.status(500).end();
+  }
+});
 
-app.get('/get_ideas', async (req, res) => {
-  sequelize.query("SELECT * FROM ideas", { type: sequelize.QueryTypes.SELECT})
-  .then(rows => {
-    res.send(rows)
+app.get('/ideas', async (req, res) => {
+  Idea.findAll().then(ideas => {   
+    res.send(ideas)
   })
    .catch(err => {
     console.error('Error: ', err);
@@ -31,34 +45,20 @@ app.get('/get_ideas', async (req, res) => {
 });
 
 
-app.post('/post_idea', bodyParser.json(), async (req, res) => {
-  const queryString = `
-  INSERT INTO ideas (
-    title,
-    place_petal,
-    water_petal,
-    energy_petal,
-    health_petal,
-    materials_petal,
-    equity_petal,
-    beauty_petal
-    ) 
-    VALUES (
-      \'${req.body.title}\',
-      \'${req.body.place_petal}\',
-      \'${req.body.water_petal}\',
-      \'${req.body.energy_petal}\',
-      \'${req.body.health_petal}\',
-      \'${req.body.materials_petal}\',
-      \'${req.body.equity_petal}\',
-      \'${req.body.beauty_petal}\'
-    )
-  `
-  try{
-    sequelize.query(queryString)
+app.post('/ideas', bodyParser.json(), async (req, res) => {
+  try {
+    Idea.create({
+      title: req.body.title,
+      place_petal: req.body.place_petal,
+      water_petal: req.body.water_petal,
+      energy_petal: req.body.energy_petal,
+      health_petal: req.body.health_petal,
+      materials_petal: req.body.materials_petal,
+      equity_petal: req.body.equity_petal,
+      beauty_petal: req.body.beauty_petal
+    });
     res.status(200).end();
-  }
-  catch(e){
+  } catch(e){
     console.log(e.stack);
     res.status(500).end();
   }
