5d4774756cbb08f9eb0a98a8e5b7a7056fe3c988
Chris Eddy <ceddy.zen@gmail.com>
2019-02-23 20:11:47 -0800

    fix fetch

--- frontend/src/components/conversations.jsx
+++ frontend/src/components/conversations.jsx
@@ -21,7 +21,7 @@ class Conversations extends Component {
 
   async getIdeas(){
     try{
-      await fetch("http://localhost:3000/ideas/", {
+      await fetch("http://localhost:3000/api/ideas/", {
           method: "GET",
           headers: {"Content-Type": "application/json"}
       }).then((response) => {
