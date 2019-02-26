c4e281959769db4cf0fe31cd5854de5f283b49fd
Chris Eddy <ceddy.zen@gmail.com>
2019-02-25 19:33:30 -0800

    fix bad conflict

--- server/database/flyway.js
+++ server/database/flyway.js
@@ -1,22 +1,10 @@
-// LOCAL DEV MIGRATION
-module.exports = {
-    url: `jdbc:postgresql://localhost:5432/postgres`,
-    schemas: 'postgres',
-    locations: 'filesystem:database/migrations',
-    user: 'postgres',
-    password: '#HelloThere69',
-    sqlMigrationSuffix: '.sql'
-};
-
-// PROD DB MIGRATION
-// const DB_PASS = process.env.DB_PASS;
-
+// // LOCAL DEV MIGRATION
 // module.exports = {
-//     url: `jdbc:postgresql://mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com:5432/postgres`,
+//     url: `jdbc:postgresql://localhost:5432/postgres`,
 //     schemas: 'postgres',
 //     locations: 'filesystem:database/migrations',
-//     user: 'master',
-//     password: DB_PASS,
+//     user: 'postgres',
+//     password: '#HelloThere69',
 //     sqlMigrationSuffix: '.sql'
 // };
 
