1925af0239491d4b3124f09229dc009a587c59a2
Chris Eddy <ceddy.zen@gmail.com>
2019-02-25 19:32:22 -0800

    Auto stash before merge of "master" and "origin/master"

--- server/database/flyway.js
+++ server/database/flyway.js
@@ -1,10 +1,22 @@
 // LOCAL DEV MIGRATION
+module.exports = {
+    url: `jdbc:postgresql://localhost:5432/postgres`,
+    schemas: 'postgres',
+    locations: 'filesystem:database/migrations',
+    user: 'postgres',
+    password: '#HelloThere69',
+    sqlMigrationSuffix: '.sql'
+};
+
+// PROD DB MIGRATION
+// const DB_PASS = process.env.DB_PASS;
+
 // module.exports = {
-//     url: `jdbc:postgresql://localhost:5432/postgres`,
+//     url: `jdbc:postgresql://mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com:5432/postgres`,
 //     schemas: 'postgres',
 //     locations: 'filesystem:database/migrations',
-//     user: 'postgres',
-//     password: '#HelloThere69',
+//     user: 'master',
+//     password: DB_PASS,
 //     sqlMigrationSuffix: '.sql'
 // };
 
