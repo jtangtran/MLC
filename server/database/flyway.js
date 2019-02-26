2514133892a5794bb9692f8b25088e93bf8846e5
Paul Sajna <sajattack@gmail.com>
2019-02-25 19:18:14 -0800

    fix flyway

--- server/database/flyway.js
+++ server/database/flyway.js
@@ -11,10 +11,11 @@
 // PROD DB MIGRATION
 const DB_PASS = process.env.DB_PASS;
 module.exports = {
-    url: `jdbc:postgresql://mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com:5432/postgres`,
-    schemas: 'postgres',
+  url: `jdbc:postgresql://mylivingcity.cilhwpqjm37r.us-west-1.rds.amazonaws.com:5432/mylivingcity`,
+    schemas: 'prod',
     locations: 'filesystem:database/migrations',
     user: 'master',
     password: DB_PASS,
-    sqlMigrationSuffix: '.sql'
+    sqlMigrationSuffix: '.sql',
+    baselineOnMigrate: true
 };
