7f099049b6775a63322b37d497f778029783604c
Chris <chris.eddy.dev@gmail.com>
2019-06-03 17:44:19 -0700

    idea page

--- frontend/src/components/idea.jsx
+++ frontend/src/components/idea.jsx
@@ -8,21 +8,24 @@ class Idea extends Component {
     super(props);
     this.state = {
       json: {},
-      idea: {}
+      idea: {},
+      comments: [],
+      newComment: ""
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleCommentChange = this.handleCommentChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.addComment = this.addComment.bind(this);
+    this.addLike = this.addLike.bind(this);
+    this.addDislike = this.addDislike.bind(this);
   }
 
-  handleChange(event){
-    this.setState({value: event.target.value});
+  handleCommentChange(event){
+    this.setState({newComment: event.target.value});
   }
 
   handleSubmit(event){
     event.preventDefault();
   }
-
-  
 
   async componentDidMount() {
     await fetch(API_URL + "/ideas/" + this.props.match.params.id)
@@ -36,36 +39,157 @@ class Idea extends Component {
       .catch(error => {
         console.log("Error: " + error);
       });
+
+    await fetch(API_URL + "/idea/" + this.props.match.params.id + '/comments')
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        this.setState({comments: json});
+      })
+      .catch(error => {
+        console.log("Error: " + error);
+      });
   }
 
+  addComment(e){
+    e.preventDefault();
+    fetch(API_URL + "/idea/" + this.props.match.params.id + '/comment', {
+      method: "POST",
+      body: {"text": this.state.newComment}
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      console.log(json);
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+    });
+  }
+
+  addLike(e){
+    e.preventDefault();
+    fetch(API_URL + "/ideas/" + this.props.match.params.id + '/upvote', {
+      method: "POST",
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      console.log(json);
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+    });
+  }
+
+  addDislike(e){
+    e.preventDefault();
+    fetch(API_URL + "/ideas/" + this.props.match.params.id + '/downvote', {
+      method: "POST"
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      console.log(json);
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+    });
+  }
 
   render() {
     return (
       <div className="Idea">
         <Navbar/>
-        <h4>{this.state.idea.title}</h4>
-        <br/>
-        <p>{this.state.json.description}</p>
-        <b>Place Petal: </b> {this.state.idea.place_petal} 
-        <br/>
-        <b>Water Petal: </b> {this.state.idea.water_petal} 
-        <br/>
-        <b>Energy Petal: </b> {this.state.idea.energy_petal} 
-        <br/>
-        <b>Health Petal: </b> {this.state.idea.health_petal} 
-        <br/>
-        <b>Materials Petal: </b> {this.state.idea.materials_petal} 
-        <br/>
-        <b>Equity Petal: </b> {this.state.idea.equity_petal} 
-        <br/>
-        <b>Beauty Petal: </b> {this.state.idea.beauty_petal} 
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Comment:
-            <textarea value={this.state.value} onChange={this.handleChange}></textarea>
-          </label>
-          <input class="btn btn-primary" type="submit" value="Submit"></input>
-        </form>
+        <div className="row">
+          <div className="col-12 text-center">
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <h1 className="display-4">{this.state.idea.title}</h1>
+                <p className="lead">{this.state.idea.description}</p>
+                <div className="row">
+                  <div className="col-6">
+                    <p className="lead">Likes</p>
+                    <h3 style={{"color": "green"}}>{this.state.json.upvoteCount}</h3>
+                    <br/>
+                    <button onClick={(e) => this.addLike(e)} type="button" className="btn btn-light">
+                      Like<span className="pl-2"></span>
+                      <span className="badge badge-success">
+                        <i className="far fa-thumbs-up"></i>
+                      </span>
+                    </button>
+                  </div>
+                  <div className="col-6">
+                    <p className="lead">Dislikes</p>
+                    <h3 style={{"color": "red"}}>{this.state.json.downvoteCount}</h3>
+                    <br/>
+                    <button onClick={(e) => this.addDislike(e)} type="button" className="btn btn-light">
+                      Dislike<span className="pl-2"></span>
+                      <span className="badge badge-danger">
+                        <i className="far fa-thumbs-down"></i>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="row">
+              <div className="col-2 text-center">
+                <p>Place Petal:</p>
+                <span className="badge badge-pill badge-primary p-2">{this.state.idea.place_petal}</span>
+              </div>
+              <div className="col-2 text-center">
+                <p>Beauty Petal:</p> 
+                <span className="badge badge-pill badge-primary p-2">{this.state.idea.beauty_petal}</span>
+              </div>
+              <div className="col-2 text-center">
+                <p>Energy Petal:</p> 
+                <span className="badge badge-pill badge-primary p-2">{this.state.idea.energy_petal}</span>
+              </div>
+              <div className="col-2 text-center">
+                <p>Health Petal:</p> 
+                <span className="badge badge-pill badge-primary p-2">{this.state.idea.health_petal}</span>
+              </div>
+              <div className="col-2 text-center">
+                <p>Materials Petal:</p> 
+                <span className="badge badge-pill badge-primary p-2">{this.state.idea.materials_petal}</span>
+              </div>
+              <div className="col-2 text-center">
+                <p>Equity Petal:</p> 
+                <span className="badge badge-pill badge-primary p-2">{this.state.idea.equity_petal}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12">
+          <br/>
+          <div className="row">
+            <div className="col-6">
+              <h3>Add Comment</h3>
+              <form className="text-center" onSubmit={(e) => this.addComment(e)}>
+                <div className="input-group">
+                  <textarea className="form-control" placeholder="Type your comment here" value={this.state.newComment} onChange={this.handleCommentChange}></textarea>
+                </div>
+                <br/>
+                <input className="btn btn-primary" type="submit" value="Post Comment"></input>
+              </form>
+            </div>
+            <div className="col-6">
+              <h3>Comments</h3>
+              <ul className="list-group">
+                {this.state.comments.map((value, index) => {
+                  return <li className="list-group-item" key={index}>{value.text}</li>
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
