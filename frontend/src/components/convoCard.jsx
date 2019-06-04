1da45200ca92d337518512d7a55d5aafce09bf31
Chris <chris.eddy.dev@gmail.com>
2019-06-04 16:46:27 -0700

    fix long descriptions

--- frontend/src/components/convoCard.jsx
+++ frontend/src/components/convoCard.jsx
@@ -11,7 +11,7 @@ class ConvoCard extends Component {
                 {this.props.model ? this.props.model.idea.title : '...'}
               </h5>
               <h6 className="card-subtitle mb-2 text-muted">Posted by: {this.props.model.idea.User.fname} {this.props.model.idea.User.lname}</h6>
-              <p className="card-text">{this.props.model.idea.description}</p>
+              <p className="card-text">{this.props.model.idea.description.length < 30 ? this.props.model.idea.description: this.props.model.idea.description.slice(0, 30) + '...'}</p>
               <div className="row">
               <div className="col-6">
                   <p className="likeText">{this.props.model.idea.upvotes} Likes</p>
