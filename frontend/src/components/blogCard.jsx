5f4c2bc775f417573d60e61b43063f7bc0d77aff
Chris <chris.eddy.dev@gmail.com>
2019-06-09 17:40:34 -0700

    fix markdown in blog cards

--- frontend/src/components/blogCard.jsx
+++ frontend/src/components/blogCard.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../stylesheets/blogs.css';
+
+const ReactMarkdown = require('react-markdown')
 const API_URL = require('../config.js')
 
 
@@ -32,7 +34,9 @@ class BlogCard extends Component {
             <img src={this.state.picture.length > 1 ? this.state.picture : 'https://c-lj.gnst.jp/public/img/common/noimage.jpg?20181011050048'} className="card-img-top" alt="..."/>
             <div className="card-body">
             <h5 className="card-title">{this.props.model.title}</h5>
-            <p className="card-text">{this.props.model.markdown ? this.props.model.markdown.slice(0, 50) + "..." : ""}</p>
+            <p className="card-text">
+              <ReactMarkdown source={this.props.model.markdown ? this.props.model.markdown.slice(0, 50) + "..." : ""}/>
+            </p>
             <a href={"/blog/" + this.props.model.id} className="btn btn-primary">Read Blog</a>
             </div>
         </div>
