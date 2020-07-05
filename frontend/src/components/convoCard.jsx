8c9f80cd721a32e83923c5844809addb261a62a9
Jason Park <qkrckdtls1120@gmail.com>
2020-07-04 22:38:26 -0700

    Chage conveCard rating

--- frontend/src/components/convoCard.jsx
+++ frontend/src/components/convoCard.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Moment from 'react-moment';
+import Ratings from 'react-ratings-declarative';
+const API_URL = require('../config.js');
 
 class ConvoCard extends Component {
 
@@ -14,13 +16,27 @@ class ConvoCard extends Component {
               </h5>
               <h6 className="card-subtitle mb-2 text-muted">Posted by: {this.props.model.idea.User.fname} {this.props.model.idea.User.lname}</h6>
               <p className="card-text">{this.props.model.idea.description.length < 30 ? this.props.model.idea.description: this.props.model.idea.description.slice(0, 30) + '...'}</p>
-              <div className="row">
-                <div className="col-6">
+              <div className="card-text">
+                {/* <div className="col-6">
                     <p className="likeText">{this.props.model.idea.upvotes} Likes</p>
                 </div>
                 <div className="col-6">
                     <p className="dislikeText">{this.props.model.idea.downvotes} Dislikes</p>
-                </div>
+                </div> */}
+                    Average Rating:
+                    <br/>
+                    <Ratings
+                      rating={this.props.model.idea.averageRating}
+                      widgetRatedColors="lightgreen"
+                      widgetEmptyColors="grey"
+                      widgetDimensions="30px"
+                    >
+                      <Ratings.Widget />
+                      <Ratings.Widget />
+                      <Ratings.Widget />
+                      <Ratings.Widget />
+                      <Ratings.Widget />
+                    </Ratings>
                 <div className="col-12">
                   <Moment format="MMM Do YYYY">{this.props.model.idea.createdAt}</Moment>
                 </div>
